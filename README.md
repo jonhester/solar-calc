b58a374aa2316cb27ffd52b438338b145d9e227f
Jon Hester <jon.d.hester@gmail.com>
2015-03-12 02:12:19 -0400

    refactor SolarCalc as class

--- README.md
+++ README.md
@@ -10,22 +10,22 @@ var solar = new SolarCalc(new Date('Mar 8 2015'),35.78,-78.649999);
 solar.sunrise // 2015-03-08T11:35:30.000Z
 ```
 ### SolarCalc functions
-each returns a `Date` object
+each is a `Date` object
 
 | Property          | Description                                                              |
 | ----------------- | ------------------------------------------------------------------------ |
-| `sunrise()`       | sunrise (top edge of the sun appears on the horizon)                     |
-| `sunriseEnd()`    | sunrise ends (bottom edge of the sun touches the horizon)                |
-| `goldenHourEnd()` | morning golden hour (soft light, best time for photography) ends         |
-| `solarNoon()`     | solar noon (sun is in the highest position)                              |
-| `goldenHour()`    | evening golden hour starts                                               |
-| `sunsetStart()`   | sunset starts (bottom edge of the sun touches the horizon)               |
-| `sunset()`        | sunset (sun disappears below the horizon, evening civil twilight starts) |
-| `dusk()`          | dusk (evening nautical twilight starts)                                  |
-| `nauticalDusk()`  | nautical dusk (evening astronomical twilight starts)                     |
-| `night()`         | night starts (dark enough for astronomical observations)                 |
-| `nightEnd()`      | night ends (morning astronomical twilight starts)                        |
-| `nauticalDawn()`  | nautical dawn (morning nautical twilight starts)                         |
-| `dawn()`          | dawn (morning nautical twilight ends, morning civil twilight starts)     |
+| `sunrise`       | sunrise (top edge of the sun appears on the horizon)                     |
+| `sunriseEnd`    | sunrise ends (bottom edge of the sun touches the horizon)                |
+| `goldenHourEnd` | morning golden hour (soft light, best time for photography) ends         |
+| `solarNoon`     | solar noon (sun is in the highest position)                              |
+| `goldenHour`    | evening golden hour starts                                               |
+| `sunsetStart`   | sunset starts (bottom edge of the sun touches the horizon)               |
+| `sunset`        | sunset (sun disappears below the horizon, evening civil twilight starts) |
+| `dusk`          | dusk (evening nautical twilight starts)                                  |
+| `nauticalDusk`  | nautical dusk (evening astronomical twilight starts)                     |
+| `night`         | night starts (dark enough for astronomical observations)                 |
+| `nightEnd`      | night ends (morning astronomical twilight starts)                        |
+| `nauticalDawn`  | nautical dawn (morning nautical twilight starts)                         |
+| `dawn`          | dawn (morning nautical twilight ends, morning civil twilight starts)     |
 
 Gladly accepting pull requests!
--- test/test.js
+++ test/test.js
@@ -21,55 +21,55 @@ describe('suncalc', function() {
     });
 
     it('get solar noon', function() {
-      assert.equal(1425835523000, solarCalc.solarNoon().getTime());
+      assert.equal(1425835523000, solarCalc.solarNoon.getTime());
     });
 
     it('get golden hour start', function() {
-      assert.equal(1425854506000, solarCalc.goldenHourStart().getTime());
+      assert.equal(1425854506000, solarCalc.goldenHourStart.getTime());
     });
 
      it('get golden hour end', function() {
-      assert.equal(1425816570000, solarCalc.goldenHourEnd().getTime());
+      assert.equal(1425816570000, solarCalc.goldenHourEnd.getTime());
     });
 
     it('get night end', function() {
-      assert.equal(1425809446000, solarCalc.nightEnd().getTime());
+      assert.equal(1425809446000, solarCalc.nightEnd.getTime());
     });
 
     it('get nautical dawn', function() {
-      assert.equal(1425811226000, solarCalc.nauticalDawn().getTime());
+      assert.equal(1425811226000, solarCalc.nauticalDawn.getTime());
     });
 
     it('get dawn', function() {
-      assert.equal(1425813000000, solarCalc.dawn().getTime());
+      assert.equal(1425813000000, solarCalc.dawn.getTime());
     });
 
     it('get sunrise', function() {
-      assert.equal(1425814530000, solarCalc.sunrise().getTime());
+      assert.equal(1425814530000, solarCalc.sunrise.getTime());
     });
 
     it('get sunriseEnd', function() {
-      assert.equal(1425814688000, solarCalc.sunriseEnd().getTime());
+      assert.equal(1425814688000, solarCalc.sunriseEnd.getTime());
     });
 
     it('get sunsetStart', function() {
-      assert.equal(1425856389000, solarCalc.sunsetStart().getTime());
+      assert.equal(1425856389000, solarCalc.sunsetStart.getTime());
     });
 
     it('get sunset', function() {
-      assert.equal(1425856548000, solarCalc.sunset().getTime());
+      assert.equal(1425856548000, solarCalc.sunset.getTime());
     });
 
     it('get dusk', function() {
-      assert.equal(1425858080000, solarCalc.dusk().getTime());
+      assert.equal(1425858080000, solarCalc.dusk.getTime());
     });
 
     it('get nautical dusk', function() {
-      assert.equal(1425859857000, solarCalc.nauticalDusk().getTime());
+      assert.equal(1425859857000, solarCalc.nauticalDusk.getTime());
     });
 
     it('get night start', function() {
-      assert.equal(1425861641000, solarCalc.nightStart().getTime());
+      assert.equal(1425861641000, solarCalc.nightStart.getTime());
     });
 
   });
@@ -92,55 +92,55 @@ describe('suncalc', function() {
     });
 
     it('get solar noon', function() {
-      assert.equal(1435075933000, solarCalc.solarNoon().getTime());
+      assert.equal(1435075933000, solarCalc.solarNoon.getTime());
     });
 
     it('get golden hour start', function() {
-      assert.equal(1439856000000, solarCalc.goldenHourStart().getTime());
+      assert.equal(1439856000000, solarCalc.goldenHourStart.getTime());
     });
 
      it('get golden hour end', function() {
-      assert.equal(1430006400000, solarCalc.goldenHourEnd().getTime());
+      assert.equal(1430006400000, solarCalc.goldenHourEnd.getTime());
     });
 
     it('get night end', function() {
-      assert.equal(1424476800000, solarCalc.nightEnd().getTime());
+      assert.equal(1424476800000, solarCalc.nightEnd.getTime());
     });
 
     it('get nautical dawn', function() {
-      assert.equal(1425859200000, solarCalc.nauticalDawn().getTime());
+      assert.equal(1425859200000, solarCalc.nauticalDawn.getTime());
     });
 
     it('get dawn', function() {
-      assert.equal(1427155200000, solarCalc.dawn().getTime());
+      assert.equal(1427155200000, solarCalc.dawn.getTime());
     });
 
     it('get sunrise', function() {
-      assert.equal(1428364800000, solarCalc.sunrise().getTime());
+      assert.equal(1428364800000, solarCalc.sunrise.getTime());
     });
 
     it('get sunriseEnd', function() {
-      assert.equal(1428451200000, solarCalc.sunriseEnd().getTime());
+      assert.equal(1428451200000, solarCalc.sunriseEnd.getTime());
     });
 
     it('get sunsetStart', function() {
-      assert.equal(1441411200000, solarCalc.sunsetStart().getTime());
+      assert.equal(1441411200000, solarCalc.sunsetStart.getTime());
     });
 
     it('get sunset', function() {
-      assert.equal(1441497600000, solarCalc.sunset().getTime());
+      assert.equal(1441497600000, solarCalc.sunset.getTime());
     });
 
     it('get dusk', function() {
-      assert.equal(1442707200000, solarCalc.dusk().getTime());
+      assert.equal(1442707200000, solarCalc.dusk.getTime());
     });
 
     it('get nautical dusk', function() {
-      assert.equal(1444003200000, solarCalc.nauticalDusk().getTime());
+      assert.equal(1444003200000, solarCalc.nauticalDusk.getTime());
     });
 
     it('get night start', function() {
-      assert.equal(1445385600000, solarCalc.nightStart().getTime());
+      assert.equal(1445385600000, solarCalc.nightStart.getTime());
     });
   });
 
